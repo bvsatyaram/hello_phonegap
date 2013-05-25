04d5e8804767fb3e2abce6e084b5e1403ad3a884
BV <bv@bv-Studio-1555.(none)>
2013-05-25 13:11:37 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HelloPhonegap::Application.config.secret_token = '92fe2f7bffca3f7f3bb791a6fb90992b71b9aea19329777b56873c05f4f8469257cab96a7103ba099cda0ca7e558c2f38aed298aa70575a2329d09651ee22f3f'
